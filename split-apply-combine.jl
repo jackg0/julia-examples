166969fbaefccfec2813ce90e662f7a22efe2b98
Jack Geissinger <jackg7@vt.edu>
2019-08-15 11:24:33 -0400

    fixing join with on parameter

--- split-apply-combine.jl
+++ split-apply-combine.jl
@@ -8,6 +8,6 @@ println(by_mean_petal_length)
 by_num_species = by(iris, :Species, N = :Species => length)
 println(by_num_species)
 
-joined = join(by_mean_petal_length, by_num_species)
+joined = join(by_mean_petal_length, by_num_species, on = :Species)
 println(joined)
 
