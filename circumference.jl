4de37052aa5be2ce2dd7c86e86de1f17ce883367
Jack Geissinger <jackg7@vt.edu>
2019-08-15 11:21:41 -0400

    initial commit of a few examples

---
+++ circumference.jl
@@ -0,0 +1,7 @@
+function circumference(radius::Float64)
+    return 2pi * radius
+end
+
+x = circumference(10.0)
+
+print(x, '\n')
---
+++ split-apply-combine.jl
@@ -0,0 +1,13 @@
+using DataFrames, CSV, Statistics
+
+iris = CSV.read(joinpath(dirname(pathof(DataFrames)), "../docs/src/assets/iris.csv"))
+
+by_mean_petal_length = by(iris, :Species, :PetalLength => mean)
+println(by_mean_petal_length)
+
+by_num_species = by(iris, :Species, N = :Species => length)
+println(by_num_species)
+
+joined = join(by_mean_petal_length, by_num_species)
+println(joined)
+
