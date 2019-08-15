808a49cd1155656eccec97aac697ec033721e306
Jack Geissinger <jackg7@vt.edu>
2019-08-15 13:32:23 -0400

    add aggregation to split-apply-combine example

--- split-apply-combine.jl
+++ split-apply-combine.jl
@@ -2,12 +2,20 @@ using DataFrames, CSV, Statistics
 
 iris = CSV.read(joinpath(dirname(pathof(DataFrames)), "../docs/src/assets/iris.csv"))
 
-by_mean_petal_length = by(iris, :Species, :PetalLength => mean)
+by_mean_petal_length = by(iris, :Species, mean_petal_length = :PetalLength => mean)
 println(by_mean_petal_length)
 
 by_num_species = by(iris, :Species, N = :Species => length)
 println(by_num_species)
 
-joined = join(by_mean_petal_length, by_num_species, on = :Species)
-println(joined)
+using_join = join(by_num_species, by_mean_petal_length, on = :Species)
+println(using_join)
 
+using_by = by(iris, :Species, N = :Species => length, mean_petal_length = :PetalLength => mean)
+println(using_by)
+
+using_aggregate = aggregate(iris, :Species, length)
+println(using_aggregate)
+
+aggregate_sum_and_mean = aggregate(iris, :Species, [sum, mean])
+println(aggregate_sum_and_mean)
